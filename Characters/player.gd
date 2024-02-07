c27d77e7a84c3c0b381359665c2cd3be6a47b88b
lazos <lazgiorgos01@gmail.com>
2024-02-08 01:41:09 +0200

    Added Player Node and Script for movement

---
+++ Characters/player.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+@export var move_speed : float = 100
+
+func _physics_process(_delta):
+    # Get input direction
+    var input_direction = Vector2(
+        Input.get_action_strength("right") - Input.get_action_strength("left"),
+        Input.get_action_strength("down") - Input.get_action_strength("up")
+    )
+
+    print(input_direction)
+
+    # Update velocity
+    velocity = input_direction * move_speed
+
+    # Move and Slide func ises velocity of character body to move character on map
+    move_and_slide()
