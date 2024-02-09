7dcff6f3950a42e0f2bf77ed993fe0589bf31654
lazos <lazgiorgos01@gmail.com>
2024-02-09 21:05:51 +0200

    Added tiles and collision and finished player movement and animation

--- Characters/player.gd
+++ Characters/player.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
 @export var move_speed : float = 100
+@export var starting_direction : Vector2 = Vector2(0,1)
 
 @onready var animation_tree = $AnimationTree
-@export var starting_direction : Vector2 = Vector2(0,1)
+@onready var state_machine = animation_tree.get("parameters/playback")
 
 
 func _ready():
@@ -24,8 +25,16 @@ func _physics_process(_delta):
     # Move and Slide func ises velocity of character body to move character on map
     move_and_slide()
 
+    pick_new_state()
+
 func update_animation_parameters(move_input : Vector2):
     # Don't change animation parameters if there is no move input
     if (move_input != Vector2.ZERO):
         animation_tree.set("parameters/idle/blend_position",move_input)
-        animation_tree.set("parameters/walk/blend_position",move_input)    
+        animation_tree.set("parameters/walk/blend_position",move_input)  
+
+func pick_new_state():
+    if (velocity!=Vector2.ZERO):
+        state_machine.travel("walk")
+    else:
+        state_machine.travel("idle")  
